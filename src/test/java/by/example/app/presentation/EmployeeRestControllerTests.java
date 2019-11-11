de414a8ad88c4ea786bbc6d93922330902f265e1
Aliaksandr3by <shpak-aliaksandr@outlook.com>
2019-11-11 06:08:07 +0300

    r10

--- src/test/java/by/example/app/presentation/EmployeeRestControllerTests.java
+++ src/test/java/by/example/app/presentation/EmployeeRestControllerTests.java
@@ -1,26 +1,15 @@
 package by.example.app.presentation;
 
 import by.example.app.infrastructure.persistence.EmployeeBeanLocalRepository;
-import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.jboss.weld.junit5.WeldJunit5Extension;
 import org.junit.Test;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.ejb.EJB;
-import javax.enterprise.context.Dependent;
-import javax.inject.Inject;
-import javax.ws.rs.core.Response;
+import static org.junit.Assert.assertNull;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(WeldJunit5Extension.class)
 public class EmployeeRestControllerTests {
-
-	@EJB
-	private EmployeeBeanLocalRepository employeeBean;
 
 	@BeforeAll
 	static void initAll() {
@@ -39,22 +28,10 @@ public class EmployeeRestControllerTests {
 	}
 
 
-	@Test
-	@Disabled("for demonstration purposes")
-	public void skippedTest() {
-		// not executed
-	}
-
-	@DisplayName("fail")
-	@Test
-	public void failTest() {
-		fail("failfailfailfailfail");
-	}
-
 	@DisplayName("should not be null")
 	@Test
 	public void cdShouldNotBeNull() {
 
-		assertNotNull("should not be null", employeeBean);
+		assertNull("should not be null", null);
 	}
 }
